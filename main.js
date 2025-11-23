8897fd81170ab110c0b7316ce8463d4f70cbefd0
Mahesa Yuztar <mahesayuztar@gmail.com>
2025-11-23 19:18:54 +0700

    php to html

--- main.js
+++ main.js
@@ -16,12 +16,35 @@ function playSoundSpesific(x) {
 }
 
 $(function () {
-    $('#board').sortable({
+  // 1. bikin array [1,2,3,4,5,6,7,8]
+  const angka = Array.from({ length: 8 }, (_, i) => i + 1);
+
+  // 2. shuffle (acak urutan)
+  angka.sort(() => Math.random() - 0.5);  // untuk 1..8 ini sudah cukup
+
+  const $board = $('#board');
+
+  // 3. generate HTML tiap tile dan append ke #board
+  angka.forEach(function (i) {
+      const src = `gambar-gambar games/mandi1_${i}.png`; // pakai / ya, jangan \ kalau path web
+
+      const tileHtml = `
+          <div class="col tile d-flex justify-content-center align-items-center border bg-light"
+               data-id="${i}">
+            <img src="${src}" class="img-fluid">
+          </div>
+      `;
+
+      $board.append(tileHtml);
+  });
+
+  // 4. setelah tile jadi, baru aktifkan sortable (kalau kamu pakai)
+  $('#board').sortable({
       items: '.tile',
       placeholder: 'tile-placeholder',
-      tolerance: 'pointer'
-    });
+      tolerance: 'pointer',
   });
+});
   
   function checkOrder() {
     const ids = $('#board .tile').map(function () {
